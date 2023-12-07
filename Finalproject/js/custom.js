f771a65cc1845715b833d93e012d726aa671250d
jal-patel <129141197+jal-patel@users.noreply.github.com>
2023-12-07 18:12:41 -0500

    Update custom.js

--- Finalproject/js/custom.js
+++ Finalproject/js/custom.js
@@ -1,10 +1,18 @@
 // function to load header and footer page
 
 
-$(document).ready(function() {
+// $(document).ready(function() {
+//     $('#header').load('./header.html');
+//     $('#footer').load('./footer.html');
+//   });
+
+if (!$('#header').html()) {
     $('#header').load('./header.html');
+}
+
+if (!$('#footer').html()) {
     $('#footer').load('./footer.html');
-  });
+}
 
 // function for menu
 
